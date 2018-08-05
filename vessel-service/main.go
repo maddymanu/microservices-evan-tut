ad7e50206c5e28291b7aeb1d5c6755ca4b7bfe47
Aditya Bansal <adbansal@ucsd.edu>
2018-08-04 18:07:35 -0700

    Adding vessel service

---
+++ vessel-service/main.go
@@ -0,0 +1,65 @@
+package main
+
+
+import (
+	pb "github.com/maddymanu/microservices-evan-tut/vessel-service/proto/vessel"
+	"errors"
+	"context"
+	"github.com/micro/go-micro"
+	"fmt"
+)
+
+type Repository interface {
+	FindAvailable(*pb.Specification) (*pb.Vessel, error)
+}
+
+type VesselRepository struct {
+	vessels []*pb.Vessel
+}
+
+func (repo *VesselRepository) FindAvailable(spec *pb.Specification) (*pb.Vessel, error) {
+	for _, vessel := range repo.vessels {
+		if spec.Capacity <= vessel.Capacity && spec.MaxWeight <= vessel.MaxWeight {
+			return vessel, nil
+		}
+	}
+	return nil, errors.New("No vessel found by that spec")
+}
+
+type service struct {
+	repo Repository
+}
+
+func (s *service) FindAvailable(ctx context.Context, req *pb.Specification, res *pb.Response) error {
+
+	// Find the next available vessel
+	vessel, err := s.repo.FindAvailable(req)
+	if err != nil {
+		return err
+	}
+
+	// Set the vessel as part of the response message type
+	res.Vessel = vessel
+	return nil
+}
+
+func main() {
+	vessels := []*pb.Vessel{
+		&pb.Vessel{Id: "vessel001", Name: "Boaty McBoatface", MaxWeight: 200000, Capacity: 500},
+	}
+	repo := &VesselRepository{vessels}
+
+	srv := micro.NewService(
+		micro.Name("go.micro.srv.vessel"),
+		micro.Version("latest"),
+	)
+
+	srv.Init()
+
+	// Register our implementation with
+	pb.RegisterVesselServiceHandler(srv.Server(), &service{repo})
+
+	if err := srv.Run(); err != nil {
+		fmt.Println(err)
+	}
+}
