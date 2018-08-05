d09a88f74f535c526c42d1258be083dd5d3ec25a
Aditya Bansal <adbansal@ucsd.edu>
2018-08-05 14:30:11 -0700

    Adding fix for Consignment service

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	pb "github.com/maddymanu/microservices-evan-tut/consignment-service/proto/consignment"
-	vesselProto "github.com/maddymanu/microservices-evan-tut/vessel-service/proto/vessel"
-	"github.com/micro/go-micro"
 	"github.com/labstack/gommon/log"
-	"fmt"
 	"os"
+	vesselProto "github.com/maddymanu/microservices-evan-tut/vessel-service/proto/vessel"
+	pb "github.com/maddymanu/microservices-evan-tut/consignment-service/proto/consignment"
+	"github.com/micro/go-micro"
+	"fmt"
 )
+
 
 const (
 	defaultHost = "localhost:27017"
@@ -36,26 +37,26 @@ func main() {
 		// We're wrapping the error returned from our CreateSession
 		// here to add some context to the error.
 		log.Panicf("Could not connect to datastore with host %s - %v", host, err)
+	}
 
-		// Create a new service. Optionally include some options here.
-		srv := micro.NewService(
+	// Create a new service. Optionally include some options here.
+	srv := micro.NewService(
 
-			// This name must match the package name given in your protobuf definition
-			micro.Name("go.micro.srv.consignment"),
-			micro.Version("latest"),
-		)
+		// This name must match the package name given in your protobuf definition
+		micro.Name("go.micro.srv.consignment"),
+		micro.Version("latest"),
+	)
 
-		vesselClient := vesselProto.NewVesselServiceClient("go.micro.srv.vessel", srv.Client())
+	vesselClient := vesselProto.NewVesselServiceClient("go.micro.srv.vessel", srv.Client())
 
-		// Init will parse the command line flags.
-		srv.Init()
+	// Init will parse the command line flags.
+	srv.Init()
 
-		pb.RegisterShippingServiceHandler(srv.Server(), &service{session, vesselClient})
+	pb.RegisterShippingServiceHandler(srv.Server(), &service{session, vesselClient})
 
-		// Run the server
-		if err := srv.Run(); err != nil {
-			fmt.Println(err)
-		}
+	// Run the server
+	if err := srv.Run(); err != nil {
+		fmt.Println(err)
 	}
 
 }
