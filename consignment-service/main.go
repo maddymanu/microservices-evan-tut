b71c2e6506815b6c0f4c18d5bccd04324c2c0fdb
Aditya Bansal <adbansal@ucsd.edu>
2018-08-04 18:12:56 -0700

    Finished Part 2

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -2,44 +2,65 @@ package main
 
 import (
 	pb "github.com/maddymanu/microservices-evan-tut/consignment-service/proto/consignment"
+	vesselProto "github.com/maddymanu/microservices-evan-tut/vessel-service/proto/vessel"
 	"context"
 	"github.com/micro/go-micro"
 	"github.com/labstack/gommon/log"
+	"fmt"
 )
 
 const (
 	port = ":50051"
 )
-
-type IRepository interface {
+type Repository interface {
 	Create(*pb.Consignment) (*pb.Consignment, error)
 	GetAll() []*pb.Consignment
-
 }
 
 // Repository - Dummy repository, this simulates the use of a datastore
 // of some kind. We'll replace this with a real implementation later on.
-type Repository struct {
+type ConsignmentRepository struct {
 	consignments []*pb.Consignment
 }
 
-func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+func (repo *ConsignmentRepository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
 	updated := append(repo.consignments, consignment)
 	repo.consignments = updated
 	return consignment, nil
 }
 
-func (repo *Repository) GetAll() []*pb.Consignment  {
+func (repo *ConsignmentRepository) GetAll() []*pb.Consignment {
 	return repo.consignments
 }
 
+// Service should implement all of the methods to satisfy the service
+// we defined in our protobuf definition. You can check the interface
+// in the generated code itself for the exact method signatures etc
+// to give you a better idea.
 type service struct {
-	repo IRepository
+	repo Repository
+	vesselClient vesselProto.VesselServiceClient
 }
 
-
-
+// CreateConsignment - we created just one method on our service,
+// which is a create method, which takes a context and a request as an
+// argument, these are handled by the gRPC server.
 func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
+
+	// Here we call a client instance of our vessel service with our consignment weight,
+	// and the amount of containers as the capacity value
+	vesselResponse, err := s.vesselClient.FindAvailable(context.Background(), &vesselProto.Specification{
+		MaxWeight: req.Weight,
+		Capacity: int32(len(req.Containers)),
+	})
+	log.Printf("Found vessel: %s \n", vesselResponse.Vessel.Name)
+	if err != nil {
+		return err
+	}
+
+	// We set the VesselId as the vessel we got back from our
+	// vessel service
+	req.VesselId = vesselResponse.Vessel.Id
 
 	// Save our consignment
 	consignment, err := s.repo.Create(req)
@@ -60,21 +81,28 @@ func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *
 	return nil
 }
 
+func main() {
 
-func main()  {
-	repo2 := &Repository{}
+	repo := &ConsignmentRepository{}
 
-	//looks like micro .New service ...
+	// Create a new service. Optionally include some options here.
 	srv := micro.NewService(
+
+		// This name must match the package name given in your protobuf definition
 		micro.Name("go.micro.srv.consignment"),
 		micro.Version("latest"),
 	)
 
+	vesselClient := vesselProto.NewVesselServiceClient("go.micro.srv.vessel", srv.Client())
+
+	// Init will parse the command line flags.
 	srv.Init()
 
-	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo2})
+	// Register handler
+	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo, vesselClient})
 
+	// Run the server
 	if err := srv.Run(); err != nil {
-		log.Error("bad")
+		fmt.Println(err)
 	}
 }
